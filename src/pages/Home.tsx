93fa412661186c1fb88ee5116c93d6d5d6df29c9
Vitor <vitorguima@gmail.com>
2021-06-23 13:11:40 -0300

    inicio implementacao do fluxo de autenticacao em Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,22 @@
+import { useContext } from 'react';
+import { TestContext } from '../App';
 import { useHistory } from 'react-router-dom';
 import ilustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 import '../styles/auth.scss';
 import { Button } from '../components/Button';
-
+import { auth, firebase } from '../services/firebase';
 
 export function Home() {
   const history = useHistory();
 
-  function navigateToNewRoom() {
-    history.push('/rooms/new');
+  function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider).then((result) => {
+      console.log(result);
+      history.push('/rooms/new');
+    })
   }
 
   return(
@@ -25,7 +31,7 @@ export function Home() {
           <img src={logoImg} alt="Letmeask"/>
           <button 
             className="create-room"
-            onClick={navigateToNewRoom}
+            onClick={handleCreateRoom}
           >
             <img src={googleIconImg} alt="Logo do google" />
             Crie sua sala com o Google
