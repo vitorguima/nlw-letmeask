0148ae688550d58c6cbe602feb6eb12eb06d027c
Vitor <vitorguima@gmail.com>
2021-06-24 11:24:47 -0300

    fluxo de criacao de salas + fluxo de entrar em uma sala criado

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import './styles/global.scss';
 import { AuthContextProvider } from './contexts/AuthContext';
+import { Room } from './pages/Room';
 
 function App() {
 
@@ -10,8 +11,11 @@ function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Route exact path="/" component={Home} />
-        <Route path="/rooms/new" component={NewRoom} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   );
